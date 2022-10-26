00c3c336bb504443a8bc3835edff6a5298d285c9
avnimungra <51969292+avnimungra@users.noreply.github.com>
2022-10-25 19:44:47 -0700

    update setting

--- people-welcome-front-end/src/Settings.jsx
+++ people-welcome-front-end/src/Settings.jsx
@@ -1,7 +1,21 @@
-function Settings() {
+import { BsSearch } from 'react-icons/bs'
+
+function SearchBar({ placeholder, data }) {
     return(
-        <h1>Settings</h1>
+        <div className="search">
+            <div className="searchInputs">
+                <input type="text" placeholder={placeholder} />
+                <div className="searchIcon">
+                    <BsSearch/>
+                </div>
+            </div>
+            <div className="dataResult">
+                {data.map((value, key) => {
+                    return <a className='dataItem' href={value.link} target='_blank'> <p>{value.title}</p> </a>
+                })}
+            </div>
+        </div>
     )
 }
 
-export default Settings
+export default SearchBar
